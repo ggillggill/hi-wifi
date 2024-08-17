b185ed4c1d60bdae2ae4bfebf0b035ec3c70689d
Teakwoo Kim <iseullee7@gmail.com>
2024-08-17 20:42:49 +0900

    sample

--- mac-map.h
+++ mac-map.h
@@ -9,11 +9,11 @@ struct MacMap : QMap<QString, QString> {
 
 
 void InitMacMap() {
-    wavMap_.insert("c2fdf3393942", "이경문.jpg");
-    imgMap_.insert("c2fdf3393942", "이경문.m4a");
+    imgMap_.insert("c2fdf3393942", "이경문.jpg");
+    wavMap_.insert("c2fdf3393942", "이경문.m4a");
 
-    wavMap_.insert("6a19dd9cf6cf", "김택우.jpg");
-    imgMap_.insert("6a19dd9cf6cf", "김택우.wav");
+    imgMap.insert("6a19dd9cf6cf", "김택우.jpg");
+    wavMap_.insert("6a19dd9cf6cf", "김택우.wav");
 
     //wavMap_.insert("ee7ec697dea6", "박현준.jpg"); // todo
     //imgMap_.insert("ee7ec697dea6", "박현준.wav"); // todo
@@ -21,8 +21,8 @@ void InitMacMap() {
     //wavMap_.insert("6cacc26b618b", "오원영.jpg"); // todo
     //imgMap_.insert("6cacc26b618b", "오원영.wav");
 
-    wavMap_.insert("42c2b145011e", "강성우.jpg");
-    imgMap_.insert("42c2b145011e", "강성우.wav");
+    imgMap_.insert("42c2b145011e", "강성우.jpg");
+    wavMap_.insert("42c2b145011e", "강성우.wav");
 }
 
 #endif // MAC-MAP_H
