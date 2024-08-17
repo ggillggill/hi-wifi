63d5cf40e59f3c9a8b9d367aa66d22581f445e97
Teakwoo Kim <iseullee7@gmail.com>
2024-08-17 22:18:01 +0900

    Update mac-map

--- src/mac-map.cpp
+++ src/mac-map.cpp
@@ -1,3 +1,71 @@
 #include "mac-map.h"
 
-extern MacMap imgMap_, wavMap_;
+MacMap imgMap_, wavMap_;
+
+void InitMacMap() {
+    imgMap_.insert("c2fdf3393942", "이경문.jpg");
+    wavMap_.insert("c2fdf3393942", "이경문.m4a");
+
+    imgMap_.insert("6a19dd9cf6cf", "김택우.jpg");
+    wavMap_.insert("6a19dd9cf6cf", "김택우.wav");
+
+    //wavMap_.insert("ee7ec697dea6", "박현준.jpg"); // todo
+    //imgMap_.insert("ee7ec697dea6", "박현준.wav"); // todo
+
+    //wavMap_.insert("6cacc26b618b", "오원영.jpg"); // todo
+    //imgMap_.insert("6cacc26b618b", "오원영.wav");
+
+    imgMap_.insert("42c2b145011e", "강성우.jpg");
+    wavMap_.insert("42c2b145011e", "강성우.wav");
+
+    imgMap_.insert("1aabcd0ee0e8", "전성현.jpg");
+    wavMap_.insert("1aabcd0ee0e8", "전성현.wav");
+
+    imgMap_.insert("ea544f2bcda8", "윤태호.jpg");
+    wavMap_.insert("ea544f2bcda8", "윤태호.wav");
+
+    imgMap_.insert("288eecadbb4d", "임학수.jpg");
+    wavMap_.insert("288eecadbb4d", "임학수.wav");
+
+    imgMap_.insert("640c9138c83e", "이승현.jpg");
+    wavMap_.insert("640c9138c83e", "이승현.wav");
+
+    imgMap_.insert("6661ef750144", "이승대.jpg");
+    wavMap_.insert("6661ef750144", "이승대.wav");
+
+    imgMap_.insert("daada476a606", "권보연.jpg");
+    wavMap_.insert("daada476a606", "권보연.wav");
+
+    imgMap_.insert("4ec404367494", "김은지.jpg");
+    wavMap_.insert("4ec404367494", "김은지.wav");
+
+    imgMap_.insert("3a3a5bda1186", "송희원.jpg");
+    wavMap_.insert("3a3a5bda1186", "송희원.wav");
+    
+    imgMap_.insert("b62d24213f92", "최유정.jpg");
+    wavMap_.insert("b62d24213f92", "최유정.wav");
+
+    imgMap_.insert("4a46ae136eac", "김현석.jpg");
+    wavMap_.insert("4a46ae136eac", "김현석.wav");
+
+    imgMap_.insert("a82bb9b7a068", "정민규.jpg");
+    wavMap_.insert("a82bb9b7a068", "정민규.wav");
+
+    imgMap_.insert("669353267f82", "서원재.jpg");
+    wavMap_.insert("669353267f82", "서원재.wav");
+
+    imgMap_.insert("163b0740fe40", "김송희.jpg");
+    wavMap_.insert("163b0740fe40", "김송희.wav");
+
+    imgMap_.insert("6ed07afbbbfd", "이새나.jpg");
+    wavMap_.insert("6ed07afbbbfd", "이새나.wav");
+
+    imgMap_.insert("56b3ca00b0ca", "문경태.jpg");
+    wavMap_.insert("56b3ca00b0ca", "문경태.wav");
+
+    imgMap_.insert("8a4fdbf693f9", "박철준.jpg");
+    wavMap_.insert("8a4fdbf693f9", "박철준.wav");
+
+    imgMap_.insert("86a2a1358471", "김태양.jpg");
+    wavMap_.insert("86a2a1358471", "김태양.wav");
+}
